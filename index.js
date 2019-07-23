9185720291f1c6472a9e451ffb8803921ac61daa
Alfred-kai <wangkaihpu@sina.com>
2019-07-23 21:05:49 +0800

    初步实现功能

--- index.js
+++ index.js
@@ -1,33 +1,66 @@
 const pkg = require("./package.json");
 const program = require("commander");
-const fs = require("fs-extra");
+const fsExt = require("fs-extra");
 const path = require("path");
-var download = require("download-git-repo");
-var exec = require("child_process").exec;
-var cli = __dirname + "/gulp replace";
+const download = require("download-git-repo");
+const workingDir = process.cwd();
+const fs = require("fs");
+const FilesList = [];
 
 program.version(pkg.version, "-v,--version");
 
 program.command("tpl <name>").action((name, args) => {
-  // fs.copySync(`${__dirname}/template`, `${workingDir}/${name}`);
-
+  fsExt.emptyDir(__dirname + "/template@@_");
   download(
     "direct:https://github.com/Alfred-kai/vue-plugin-template.git",
-    `${__dirname}/template`,
+    `${__dirname}/template@@_`,
     { clone: true },
     function(err) {
       console.log(err ? "Error" : "Success");
-      console.log("cli is : " + cli);
-      exec(cli, { encoding: "utf8" }, function(err, stdout, stderr) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log("stdout" + stdout);
-        console.log("stderr" + stderr);
+      readFileList(__dirname + "/template@@_/", FilesList);
+
+      FilesList.map(item => {
+        handleFiles(item.path, item.fullPath, name);
       });
     }
   );
 });
 
 program.parse(process.argv);
+
+function readFileList(path, filesList) {
+  var files = fs.readdirSync(path);
+  files.forEach(function(itm, index) {
+    var stat = fs.statSync(path + itm);
+    if (stat.isDirectory()) {
+      readFileList(path + itm + "/", filesList);
+    } else {
+      var obj = {};
+      obj.path = path; //路径
+      obj.filename = itm; //名字
+      obj.fullPath = path + itm; //全路径
+      filesList.push(obj);
+    }
+  });
+}
+
+function handleFiles(path, fullPath, name) {
+  fs.readFile(fullPath, "utf8", function(err, files) {
+    var result = files.replace(/@BASE_URL/g, "wangkai");
+
+    var idx = path.indexOf("@@_");
+    var pathPostFix = path.substring(idx + 3);
+    var fullPathPostFix = fullPath.substring(idx + 3);
+
+    var path2 = workingDir + "/" + name + pathPostFix;
+    var fullPath2 = workingDir + "/" + name + fullPathPostFix;
+
+    if (!fs.existsSync(path2)) {
+      fs.mkdirSync(path2);
+    }
+
+    fs.appendFile(fullPath2, result, "utf8", function(err) {
+      if (err) return console.log(err);
+    });
+  });
+}
